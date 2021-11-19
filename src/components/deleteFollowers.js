dac9c197897cc175fb2491a734870241d1de9590
RohitSingh104 <63583088+rksingh001@users.noreply.github.com>
2021-11-19 11:09:51 +0530

    delete selected

---
+++ src/components/deleteFollowers.js
@@ -0,0 +1,31 @@
+import React  from 'react';
+import Button from '@mui/material/Button';
+
+const DeleteFollowers = ({list , checkedElement , listSetter}) => {
+    // console.log(list);
+    // console.log(checkedElement);
+    // console.log(listSetter);
+    const deleteSelectedElement = () =>{
+        const result = list.filter((currElement , index) =>{
+            return !checkedElement.includes(currElement.id);
+        })
+        listSetter(result);
+        console.log(list);
+    } 
+    
+    return ( 
+       
+        <div className="delete-follower">
+            <Button variant="contained" color="error"
+                sx={{marginTop:5 , marginLeft:20}}
+                onClick={deleteSelectedElement}
+            >
+                Delete Selected
+            </Button>
+        
+            
+        </div>
+    )
+}
+
+export default DeleteFollowers;
